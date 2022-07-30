7f15e831e4dc5b2c57293df978791ae7ec7ed238
nateesh <nathan.eshraghi@gmail.com>
2022-07-30 10:27:52 +1000

    App main, apollo client

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,63 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import { ApolloClient } from "@apollo/client/core/ApolloClient.js";
+    import { InMemoryCache } from "@apollo/client/cache/inmemory/inMemoryCache.js";
+    import { setClient } from "svelte-apollo";
+    import Nav from "./lib/Nav.svelte";
+    import Products from "./lib/Products.svelte";
+    import Sidebar from "./lib/Sidebar.svelte";
+    import Footer from "./lib/Footer.svelte";
+
+    let secret =
+        "NFFWU4JhKWpABBh2iK7a54Ika6slfPYXjeIj0EoCjjp8LwZFml7z85as8HBfgAr8";
+
+    const client = new ApolloClient({
+        uri: "https://guided-seahorse-31.hasura.app/v1/graphql",
+        headers: {
+            "content-type": "application/json",
+            "x-hasura-admin-secret": secret,
+        },
+        cache: new InMemoryCache(),
+    });
+
+    setClient(client);
+
+    export function refetchProducts() {}
+
+    let products;
+    let selectedCategory;
+    function handleCatSelect(event) {
+        selectedCategory = event.detail;
+        console.log(selectedCategory);
+        products.refetchProducts(selectedCategory);
+    }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="">
+    <div class="p-7 pb-60 ">
+        <div class="grid items-center justify-center">
+            <div class="">
+                <Nav on:category-select={handleCatSelect} />
+            </div>
+            <div class="grid grid-cols-4">
+                <div
+                    class="h-64 min-h-full col-span-3 overflow-y-auto scroll-smooth snap-both"
+                >
+                    <Products {selectedCategory} bind:this={products} />
+                </div>
+                <div class="col-span-1">
+                    <Sidebar />
+                </div>
+            </div>
+            <div class="">
+                <Footer />
+            </div>
+        </div>
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    main {
+        background-color: rgb(131, 163, 76);
+        height: 100%;
+    }
 </style>
